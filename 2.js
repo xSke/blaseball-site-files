78a10fadfc5372fcdad7cddd6e29cfd9229467d0
The Game Band <dontmailthis@example.com>
2020-10-19 22:00:00 +0000

    Site update @ 2020-10-19T22:00:00.000Z

--- 2.js
+++ 2.js
@@ -1,4 +1,4 @@
-/*! For license information please see 2.44f70c9f.chunk.js.LICENSE.txt */
+/*! For license information please see 2.6b9a1664.chunk.js.LICENSE.txt */
 (this.webpackJsonpsite = this.webpackJsonpsite || []).push([
   [2],
   [
